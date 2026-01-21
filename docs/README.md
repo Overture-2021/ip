22da976947f84cb7299fbc087d8508485d5dd5d3
Overture-2021 <e1482085@u.nus.edu>
2026-01-21 16:35:56 +0800

    Change agent name and add greeting/farewell

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Kanade User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " _  __                     _      \n"
+                + "| |/ /                    | |     \n"
+                + "| ' /  __ _ _ __   __ _  __| | ___ \n"
+                + "|  <  / _` | '_ \\ / _` |/ _` |/ _ \\\n"
+                + "| . \\| (_| | | | | (_| | (_| |  __/\n"
+                + "|_|\\_\\\\__,_|_| |_|\\__,_|\\__,_|\\___|\n";
+        System.out.println("_________________________");
+        System.out.println("Initiating...\n" + logo);
+        Message greet = new Message("Hello! I'm Kanade\nWhat can I do for you?");
+        Message farewell = new Message("Bye. Hope to see you again soon!");
     }
 }
+
+
