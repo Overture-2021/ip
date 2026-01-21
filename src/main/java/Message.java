db9d928597ad6c44939326e711bcd07266d65737
Overture-2021 <e1482085@u.nus.edu>
2026-01-21 16:35:56 +0800

    Changed agent name and added greeting/farewell

---
+++ src/main/java/Message.java
@@ -0,0 +1,8 @@
+public class Message {
+    private String text;
+    public Message(String output) {
+        text = output;
+        System.out.println(text);
+        System.out.println("_________________________");
+    }
+}
