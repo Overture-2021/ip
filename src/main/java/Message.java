91ec4a3d93a3f722397d6b60f492932cf42d3b49
Overture-2021 <e1482085@u.nus.edu>
2026-01-26 17:08:15 +0800

    Implemented Echo

--- src/main/java/Message.java
+++
@@ -1,8 +0,0 @@
-public class Message {
-    private String text;
-    public Message(String output) {
-        text = output;
-        System.out.println(text);
-        System.out.println("_________________________");
-    }
-}
