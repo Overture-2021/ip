8f10b2af0010f9db2a868392d56325f0f60942b9
Overture-2021 <e1482085@u.nus.edu>
2026-01-26 17:08:15 +0800

    Implement echo

--- src/main/java/Kanade.java
+++ src/main/java/Kanade.java
@@ -1,15 +1,40 @@
+import java.util.Scanner;
+
 public class Kanade {
-    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    public Kanade(){
         String logo = " _  __                     _      \n"
                 + "| |/ /                    | |     \n"
                 + "| ' /  __ _ _ __   __ _  __| | ___ \n"
                 + "|  <  / _` | '_ \\ / _` |/ _` |/ _ \\\n"
                 + "| . \\| (_| | | | | (_| | (_| |  __/\n"
                 + "|_|\\_\\\\__,_|_| |_|\\__,_|\\__,_|\\___|\n";
-        System.out.println("_________________________");
+
         System.out.println("Initiating...\n" + logo);
-        Message greet = new Message("Hello! I'm Kanade\nWhat can I do for you?");
-        Message farewell = new Message("Bye. Hope to see you again soon!");
+    }
+
+    public void Echo(){
+        String ln = "";
+        while(!ln.equals("bye")){
+            ln = sc.nextLine();
+            if(ln.equals("bye")){
+                PrintMsg("Bye. Hope to see you again soon!");
+                break;
+            }
+            PrintMsg(ln);
+        }
+    }
+
+    public void PrintMsg(String input){
+        System.out.println("_________________________");
+        System.out.println(input);
+        System.out.println("_________________________");
+    }
+    public static void main(String[] args) {
+        Kanade k423 = new Kanade();
+        k423.PrintMsg("Hello! I'm Kanade\nWhat can I do for you?");
+        k423.Echo();
+
     }
 }
 
