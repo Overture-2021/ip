a49fa73f74afcfb8c835fa5be4bcebf823487850
Overture-2021 <e1482085@u.nus.edu>
2026-01-27 21:24:44 +0800

    Implemented mark/unmark & Personalization & Coding Standards

--- src/main/java/Kanade.java
+++ src/main/java/Kanade.java
@@ -4,56 +4,63 @@ public class Kanade {
     Scanner sc = new Scanner(System.in);
     private Task[] Tasks = new Task[100];
     private Integer numTask;
-    public Kanade(){
+
+    public Kanade() {
         String logo = " _  __                     _      \n"
                 + "| |/ /                    | |     \n"
                 + "| ' /  __ _ _ __   __ _  __| | ___ \n"
                 + "|  <  / _` | '_ \\ / _` |/ _` |/ _ \\\n"
                 + "| . \\| (_| | | | | (_| | (_| |  __/\n"
-                + "|_|\\_\\\\__,_|_| |_|\\__,_|\\__,_|\\___|\n";
+                + "|_|\\_\\\\__,_|_| |_|\\__,_|\\__,_|\\___|❤\n";
 
         System.out.println("Initiating...\n" + logo);
-        numTask=0;
+        this.PrintMsg("Ciallo～(∠・ω< )⌒★)! I'm Kanade!\n こんにちは！私の名前は奏（かなで）です！よろしくお願いします！");
+        numTask = 0;
     }
 
-    public void AddEvent(){
+    public void AddEvent() {
         String ln = "";
-        while(true){
+        Integer target;
+        while (true) {
             ln = sc.nextLine();
-            if(ln.equals("bye")){
-                PrintMsg("Bye. Hope to see you again soon!");
+            if (ln.equals("bye")) {
+                PrintMsg("Bye. Hope to see you again soon!\nまたね！");
                 break;
-            }
-
-            if(ln.equals("list")){
+            } else if (ln.equals("list")) {
                 PrintTasks();
-            }
-            else{
+            } else if (ln.contains("unmark ")) {
+                target = Integer.parseInt(ln.replace("unmark ", ""));
+                Tasks[target].setStatus(false);
+            } else if (ln.contains("mark ")) {
+                target = Integer.parseInt(ln.replace("mark ", ""));
+                Tasks[target].setStatus(true);
+            } else {
                 Tasks[numTask] = new Task(ln);
-                numTask+=1;
+                numTask += 1;
                 PrintMsg("added: " + ln);
             }
 
         }
     }
 
-    public void PrintTasks(){
+    public void PrintTasks() {
         System.out.println("_________________________");
-        Integer i=0;
-        for(i=0;i<numTask;i+=1){
-            System.out.println(i.toString() + ". " + Tasks[i].description);
+        Integer i = 0;
+        for (i = 0; i < numTask; i += 1) {
+            System.out.println(i.toString() + ".[" + Tasks[i].getStatusIcon() + "] " + Tasks[i].description);
         }
         System.out.println("_________________________");
     }
 
-    public void PrintMsg(String input){
+    public void PrintMsg(String input) {
         System.out.println("_________________________");
         System.out.println(input);
         System.out.println("_________________________");
     }
+
     public static void main(String[] args) {
         Kanade k423 = new Kanade();
-        k423.PrintMsg("Hello! I'm Kanade\nWhat can I do for you?");
+
         k423.AddEvent();
 
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,10 +7,22 @@ public class Task {
         this.isDone = false;
     }
 
-
+    public void setStatus(boolean newStatus) {
+        System.out.println("_________________________");
+        if (newStatus) {
+            this.isDone = true;
+            System.out.println("Nice! I've marked this task as done");
+            System.out.println("[X] " + description);
+        } else {
+            this.isDone = false;
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("[ ] " + description);
+        }
+        System.out.println("_________________________");
+    }
 
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "X" : " "); // mark done task with ss
     }
 
 }
